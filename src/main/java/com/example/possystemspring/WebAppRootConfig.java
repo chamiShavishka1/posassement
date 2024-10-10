d215b6cf846f330caa72b9e9d04d0a0d0b326e7b
chamiShavishka1 <chamishavishka5@gmail.com>
2024-10-10 19:13:03 +0530

    webConfig

---
+++ src/main/java/com/example/possystemspring/WebAppRootConfig.java
@@ -0,0 +1,48 @@
+package com.example.possystemspring;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+public class WebAppRootConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        var dbms = new DriverManagerDataSource();
+        dbms.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dbms.setUrl("jdbc:mysql://localhost:3306/pos_javaee?createDatabaseIfNotExist=true");
+        dbms.setUsername("root");
+        dbms.setPassword("Ijse@1234");
+        return dbms;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("lk.ijse.posbackend.entity");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
